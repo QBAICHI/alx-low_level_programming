5c458ec949db6b28f7b0e2841d5fe3363c88ad7c
QBAICHI <qbaichi.work@gmail.com>
2023-03-21 16:01:42 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,21 @@
 #include "main.h"
 
-
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
+/**
+ * print_alphabet_x10 - write the the lower-case alphabet on a line.
+ *
+ */
 	void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10 ; i++)
 	{
-		char ch;
-		int i;
-
-
-		i = 0;
-
-
-		while (i < 10)
+		for (c = 'a' ; c <= 'z' ; c++)
 		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
+			_putchar(c);
 		}
+		_putchar('\n');
 	}
+}
+
